05015f16cc9257030aa1a8a32832bd677f8af5f5
Pedro2g21 <pedrogasperin24@gmail.com>
2025-04-11 20:43:30 -0300

    Update menukk.lua

--- menukk.lua
+++ menukk.lua
@@ -1,14 +1,10 @@
 local imgui = require 'imgui' -- загружаем библиотеку
 
--- в этой функции осуществляется вся работа с ImGui
--- она вызывается каждый кадр, но только если imgui.Process равен true
 function imgui.OnDrawFrame()
-  imgui.Begin('My window') -- новое окно с заголовком 'My window'
-  imgui.Text('Hello world') -- простой текст внутри этого окна
-  imgui.End() -- конец окна
+  imgui.Begin('My window')
+  imgui.End() 
 end
 
 function main()
-  imgui.Process = true -- ImGui будет обрабатываться, пока imgui.Process равен true
-  -- в этом примере мы просто активируем ImGui сразу же после загрузки игры
+  imgui.Process = true 
 end
