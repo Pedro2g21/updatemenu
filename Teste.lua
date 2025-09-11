d70af1c8cdfeb528f60728d6deb1a7dd09dde540
Pedro2g21 <pedrogasperin24@gmail.com>
2025-09-11 10:11:15 -0300

    Update Teste.lua

--- Teste.lua
+++ Teste.lua
@@ -1,24 +1,14 @@
-local imgui = require 'imgui'
-local key = require 'vkeys'
+local imgui = require 'mimgui'
 
-
-local main_window_state = imgui.ImBool(false)
-function imgui.OnDrawFrame()
-  if main_window_state.v then 
-    imgui.SetNextWindowSize(imgui.ImVec2(150, 200), imgui.Cond.FirstUseEver) 
-
-    imgui.Begin('My window', main_window_state)
-    imgui.End()
-  end
-end
+local newFrame = imgui.OnFrame(
+    function() return true end,
+    function(player)
+        imgui.Begin("Main Window")
+        imgui.Text("Hello")
+        imgui.End()
+    end
+)
 
 function main()
-  while true do
-    wait(0)
-    if wasKeyPressed(key.VK_X) then 
-        main_window_state.v = not main_window_state.v 
-    end
-    imgui.Process = main_window_state.v 
-  end
+    wait(1)
 end
-
